17e6fed0ff5f3987fb49b75459061cf27a26fa3c
jgmv <jg.macia@gmail.com>
2020-06-27 15:08:15 +0200

    extract strain name in sequence header

--- get_fasta_from_gb.py
+++ get_fasta_from_gb.py
@@ -4,7 +4,7 @@
     File name: get_metadata_from_gb.py
     Author: Jose G. Macia-Vicente
     Date created: 2014-11-18
-    Date last modified: 2019-07-25
+    Date last modified: 2020-06-27
     Python Version: 2.7
 '''
 
@@ -25,22 +25,39 @@ parser.add_argument(
     type=str,
     default='outSeq.fasta')
 
+parser.add_argument(
+    "-accession", help = 'output sequence accession ("acc", default) or strain ("str")',
+    type=str,
+    default='acc')
+
 args = parser.parse_args()
 
 of = args.o
 
 if1 = args.gb
 of1 = args.o
+seqname = args.accession
 
 count = 0
 
 handle = open(if1)
 seqOut = open(of1, "w")
 
+if seqname != 'acc' and seqname != 'str':
+	print "'accession' must be 'acc' or 'str'"
+	sys.exit()
+
 for seq_record in SeqIO.parse(handle, "genbank"):
 	accno = seq_record.name
 	organism = "_".join(seq_record.annotations["organism"].split())
-	seqOut.write(">"+accno+"_"+organism+"\n")
+	if seqname is 'acc':
+		seqOut.write(">"+accno+"_"+organism+"\n")
+	else:
+		features = seq_record.features[0]
+		strain = "_".join(str(features.qualifiers.get("strain"))[2:-2].split())
+		if strain == "":
+			strain = "NA"
+		seqOut.write(">"+organism+"_"+strain+"\n")		
 	seqOut.write(str(seq_record.seq.lower())+"\n")		
 
 	count +=1
