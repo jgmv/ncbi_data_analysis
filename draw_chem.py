35d02a90f53db253097cb640bfd72fad7ee12768
jgmv <53367553+jgmv@users.noreply.github.com>
2025-03-20 16:20:12 +0100

    Update draw_chem.py

--- draw_chem.py
+++ draw_chem.py
@@ -4,54 +4,69 @@
     File name: draw_chem.py
     Author: Jose G. Macia-Vicente
     Date created: 2017-03-03
-    Date last modified: 2017-03-03
-    Python Version: 2.7
+    Date last modified: 2025-03-20
 '''
 
 import argparse
-from rdkit.Chem import AllChem as Chem
+from rdkit import Chem
+from rdkit.Chem import rdDepictor
 from rdkit.Chem import Draw
+from rdkit.Chem.Draw import MolDrawing
+from rdkit.Chem.Draw.MolDrawing import DrawingOptions
+import rdkit
 
 # input arguments parameters
-
 parser = argparse.ArgumentParser(
-    description='Draw chemical structures from provided file.')
+    description='Draw simple chemical structures from SMILEs.')
 
 parser.add_argument(
-    "cid",
-    help = 'input CID from list')
+    "-s",
+    type=str,
+    default="",
+    help = 'input single SMILEs in-line')
 
 parser.add_argument(
-    "-i", help = 'input file',
+    "-f", help = 'input file with names and SMILEs (tab-separated) in rows',
     type=str,
     default='smiles.txt')
 
 parser.add_argument(
     "-o", help = 'output file',
     type=str,
-    default='output.pdf')
+    default='output.svg')
+
+parser.add_argument(
+    "-size", help = 'size in pixels',
+    type=int,
+    default=300)
+
+parser.add_argument(
+    "--stereo", help = 'show stereo bonds',
+    action=argparse.BooleanOptionalAction)
 
 args = parser.parse_args()
 
+# set options (not working)
+#opt = Draw.MolDrawing.DrawingOptions()
+
+# define drawings
+def draw_mol(smile):
+    d = Chem.MolFromSmiles(smile)
+    rdDepictor.Compute2DCoords(d)
+    rdDepictor.StraightenDepiction(d)
+    return(d)
+    
 # process query
-handle = open(args.i)
+if args.s == "":
+    handle = open(args.f)
+    for line in handle:
+        name = line.split()[0]
+        smile = draw_mol(line.split()[1])
+        Draw.MolToFile(smile, name + '.svg', size = (args.size, args.size), \
+            wedgeBonds = args.stereo)
+else:
+    smile = draw_mol(args.s)
+    Draw.MolToFile(smile, args.o, size = (args.size, args.size), \
+        wedgeBonds = args.stereo)
 
-# for black and white (comment for color):
-opt = Draw.DrawingOptions()
-from collections import defaultdict
-dd = defaultdict(lambda:(0,0,0))  # <- use a default color of black
-opt.elemDict = dd
-
-# draw compound
-for line in handle:
-  if line.split()[0] in args.cid:
-    smile = Chem.MolFromSmiles(line.split()[1])
-    #smile = Chem.AddHs(smile)
-    Chem.Compute2DCoords(smile)
-    #Chem.EmbedMolecule(smile)
-    #Chem.MMFFOptimizeMolecule(smile)
-    #Chem.UFFOptimizeMolecule(smile)
-    #smile = Chem.RemoveHs(smile)
-    Draw.MolToFile(smile, args.o, options = opt)
-
-
+# end
